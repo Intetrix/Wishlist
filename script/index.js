f661d693b4122bd5cb0fc2225d30fb697e0973ee
Tanya Sevko <taniasevko@yandex.ru>
2024-10-29 15:28:10 +0300

    feat: add modal event listener

--- script/index.js
+++ script/index.js
@@ -1,3 +1,9 @@
 import { createModalWindow } from "./modal-window.js";
 
-createModalWindow ();
+const openModal = document.querySelectorAll('.open_modal');
+
+openModal.forEach(el => {
+    el.addEventListener('click', () => {
+        createModalWindow ();
+    })
+});
